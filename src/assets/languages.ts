eb9288043cc09374e9a9169874d5e8f55185e6bf
Dheeraj <131849765+17sdheeraj@users.noreply.github.com>
2024-05-30 19:42:53 +0530

    Update languages.ts

--- src/assets/languages.ts
+++ src/assets/languages.ts
@@ -1,99 +1,53 @@
-import ar from "@/assets/locales/en.json";
-import bg from "@/assets/locales/en.json";
-import bn from "@/assets/locales/en.json";
-import ca from "@/assets/locales/en.json";
-import caVl from "@/assets/locales/en.json";
-import cat from "@/assets/locales/en.json";
-import cs from "@/assets/locales/en.json";
-import de from "@/assets/locales/en.json";
-import el from "@/assets/locales/en.json";
 import en from "@/assets/locales/en.json";
-import es from "@/assets/locales/en.json";
-import et from "@/assets/locales/en.json";
-import fa from "@/assets/locales/en.json";
-import fi from "@/assets/locales/en.json";
-import fr from "@/assets/locales/en.json";
-import gl from "@/assets/locales/en.json";
-import gu from "@/assets/locales/en.json";
-import he from "@/assets/locales/en.json";
-import hi from "@/assets/locales/en.json";
-import id from "@/assets/locales/en.json";
-import is from "@/assets/locales/en.json";
-import it from "@/assets/locales/en.json";
-import ja from "@/assets/locales/en.json";
-import ko from "@/assets/locales/en.json";
-import lv from "@/assets/locales/en.json";
-import minion from "@/assets/locales/en.json";
-import ne from "@/assets/locales/en.json";
-import nl from "@/assets/locales/en.json";
-import nv from "@/assets/locales/en.json";
-import pa from "@/assets/locales/en.json";
-import pirate from "@/assets/locales/en.json";
-import pl from "@/assets/locales/en.json";
-import ptbr from "@/assets/locales/en.json";
-import ptpt from "@/assets/locales/en.json";
-import ro from "@/assets/locales/en.json";
-import ru from "@/assets/locales/en.json";
-import sl from "@/assets/locales/en.json";
-import sv from "@/assets/locales/en.json";
-import ta from "@/assets/locales/en.json";
-import th from "@/assets/locales/en.json";
-import tok from "@/assets/locales/en.json";
-import tr from "@/assets/locales/en.json";
-import uk from "@/assets/locales/en.json";
-import uwu from "@/assets/locales/en.json";
-import vi from "@/assets/locales/en.json";
-import zhhant from "@/assets/locales/en.json";
-import zh from "@/assets/locales/en.json";
 
 export const locales = {
   en,
-  ca,
-  "ca-ES": caVl,
-  cs,
-  de,
-  fr,
-  it,
-  nl,
-  pl,
-  tr,
-  vi,
-  zh,
-  he,
-  sv,
-  pirate,
-  cat,
-  uwu,
-  minion,
-  lv,
-  th,
-  ne,
-  ar,
-  es,
-  et,
-  tok,
-  hi,
-  "pt-BR": ptbr,
-  "pt-PT": ptpt,
-  uk,
-  bg,
-  bn,
-  el,
-  fa,
-  gu,
-  id,
-  ja,
-  ko,
-  sl,
-  ta,
-  "zh-Hant": zhhant,
-  is,
-  ru,
-  gl,
-  pa,
-  ro,
-  fi,
-  nv,
+  ca: en,
+  "ca-ES": en,
+  cs: en,
+  de: en,
+  fr: en,
+  it: en,
+  nl: en,
+  pl: en,
+  tr: en,
+  vi: en,
+  zh: en,
+  he: en,
+  sv: en,
+  pirate: en,
+  cat: en,
+  uwu: en,
+  minion: en,
+  lv: en,
+  th: en,
+  ne: en,
+  ar: en,
+  es: en,
+  et: en,
+  tok: en,
+  hi: en,
+  "pt-BR": en,
+  "pt-PT": en,
+  uk: en,
+  bg: en,
+  bn: en,
+  el: en,
+  fa: en,
+  gu: en,
+  id: en,
+  ja: en,
+  ko: en,
+  sl: en,
+  ta: en,
+  "zh-Hant": en,
+  is: en,
+  ru: en,
+  gl: en,
+  pa: en,
+  ro: en,
+  fi: en,
+  nv: en,
 };
 
 export type Locales = keyof typeof locales;
